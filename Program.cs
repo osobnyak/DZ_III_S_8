50c8f840ea6f58e526521c4d82d712d49ea45a0d
Vladimir.S <vibiray2@mail.ru>
2023-01-20 23:09:01 +0400

    62 saved

--- Program.cs
+++ Program.cs
@@ -1,29 +1,68 @@
-﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
-// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
 
-// Массив размером 2 x 2 x 2
+int[,] num = new int[4, 4];
+num[0, 0] = 1;
+int sum = 0;
 
-// 66(0,0,0) 25(0,1,0)
-// 34(1,0,0) 41(1,1,0)
-// 27(0,0,1) 90(0,1,1)
-// 26(1,0,1) 55(1,1,1)
-
-int[,,] num = new int[2, 2, 2];
-
-for (int i = 0; i < num.GetLength(0); i++)
+for (int i = 0; i < 1; i++) // 1-4
 {
     for (int j = 0; j < num.GetLength(1); j++)
     {
-        for (int k = 0; k < num.GetLength(2); k++)
-        {
-            foreach (int a in num) // не уверен, что этот цикл исключит повторы.
-            {
-                if (a == num[i, j, k]) continue; 
-                num[i, j, k] = new Random().Next(10, 100); 
-            } 
-            Console.Write($"{num[i, j, k]} ({i},{j},{k}) ");
-        }
-        Console.WriteLine();
+        sum = sum + 1;
+        num[i, j] = sum;
     }
 }
 
+for (int i = 1; i < num.GetLength(0); i++) // 5-7
+{
+    for (int j = 3; j <= 3; j++)
+    {
+        sum = sum + 1;
+        num[i, j] = sum;
+    }
+}
+
+for (int i = 3; i <= 3; i++) // 8-10
+{
+    for (int j = 2; j >= 0; j--)
+    {
+        sum = sum + 1;
+        num[i, j] = sum;
+    }
+}
+
+for (int i = 2; i > 0; i--) // 11-12
+{
+    for (int j = 0; j >= 0; j--)
+    {
+        sum = sum + 1;
+        num[i, j] = sum;
+    }
+}
+
+for (int i = 1; i <= 1; i++) // 13-14
+{
+    for (int j = 1; j < 3; j++)
+    {
+        sum = sum + 1;
+        num[i, j] = sum;
+    }
+}
+
+num[2, 2] = num[1, 2] + 1; // 15-16
+num[2, 1] = num[2, 2] + 1;
+
+for (int i = 0; i < num.GetLength(0); i++) // демонстрация массива
+{
+    for (int j = 0; j < num.GetLength(1); j++)
+    {
+        Console.Write($"{num[i, j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
